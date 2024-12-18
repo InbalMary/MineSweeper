bb48f5329b6e465c44999188087eaf184d99b73e
InbalMary <inbalby10@gmail.com>
2024-12-18 15:57:36 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 'use strict'
 
 // Board size
-const ROWS = 4
-const COLS = 4
+var ROWS = 4
+var COLS = 4
+var NUM_OF_MINES = 2
 
 // Game elements
 const MINE = '💣'
+const FLAG = '🚩'
+const EXPLOAD = '💥'
 
 var gBoard
+var gMinesPoss
+var gMinesCounter
 
 function onInitGame() {
     gBoard = buildBoard()
-    // setRandMinesPoss(gBoard, 2)
+    gMinesPoss = setRandMinesPoss(gBoard, NUM_OF_MINES)
+    gMinesCounter = NUM_OF_MINES
     setMinesNegsCount(gBoard)
     renderBoard(gBoard)
 }
@@ -30,7 +36,7 @@ function buildBoard() {
         }
     }
     // //Place two mimes
-    board[0][1].isMine = board[2][2].isMine = true
+    // board[0][1].isMine = board[2][2].isMine = true
 
     // console.log(board)
     return board
@@ -51,11 +57,8 @@ function renderBoard(board) {
             if (currCell.minesAroundCount) cellClass += ' number'
             if (!currCell.isMine && !currCell.minesAroundCount) cellClass += ' empty'
 
-            strHTML += `\t<td class="cell ${cellClass}" onclick="onCellClicked(this, ${i}, ${j})">`
-
-            // if (currCell.isMine) strHTML += MINE
-            // if(currCell.minesAroundCount) strHTML += currCell.minesAroundCount
-
+            strHTML += `\t<td class="cell ${cellClass}" onclick="onCellClicked(this, ${i}, ${j})" 
+                                oncontextmenu="onRightCellClicked(this, ${i}, ${j})">`
             strHTML += '</td>\n'
         }
         strHTML += '</tr>\n'
@@ -100,29 +103,78 @@ function countNegs(cellI, cellJ, mat) {
 function onCellClicked(elCell, i, j) {
     const cell = gBoard[i][j]
     console.log('cell', cell, i, j)
-    cell.isShown = true
-
-    // model update
-    if (cell.isMine) {
-        console.log('Game Over')
-        elCell.innerText = MINE
+    if (!cell.isShown && !cell.isMarked) {
+        if (cell.isMine) {
+            console.log('Game Over')
+            elCell.innerText = MINE
+            gameOver(elCell, i, j)
+        } else {
+            if (cell.minesAroundCount) elCell.innerText = cell.minesAroundCount;
+        }
+        cell.isShown = true
+        if (gMinesCounter === 0) {
+            checkGameOver()
+        }
     }
-    else if (!cell.isMine && !cell.isMarked) {
-        elCell.innerText = cell.minesAroundCount
-    }
-    //DOM update
 
     cell.isMarked = true
     console.log('negscount', cell.minesAroundCount)
 }
 
+function onRightCellClicked(elCell, i, j) {
+    console.log('gMinesCounter', gMinesCounter)
+    const cell = gBoard[i][j]
+    console.log('cell', cell, i, j)
+
+    if (!cell.isShown) {
+        if (!cell.isMarked) {
+            elCell.innerText = FLAG
+            if (cell.isMine) gMinesCounter--
+        } else {
+            elCell.innerText = ''
+            if (cell.isMine) gMinesCounter++
+        }
+        cell.isMarked = !cell.isMarked
+    }
+    if (gMinesCounter === 0) {
+        checkGameOver()
+    }
+}
+
+//hide the menu
+document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+});
+
+function gameOver() {
+    for (var k = 0; k < gMinesPoss.length; k++) {
+        // const mine = gMinesPoss[k]
+        const elMine = document.querySelector(`.cell-${gMinesPoss[k].i}-${gMinesPoss[k].j}`)
+        // console.log('elMine', elMine)
+        elMine.innerText = EXPLOAD
+    }
+}
 
 function onCellMarked(elCell) {
 
 }
 
 function checkGameOver() {
-
+    console.log('checVic')
+    var flag = 1
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            var cell = gBoard[i][j]
+            if ((cell.isMine && cell.isMarked) || (!cell.isMine && cell.isShown)) {
+                flag = 1
+            } else {
+                console.log('nooooo')
+                return false
+            }
+        }
+    }
+    console.log('YOU WON')
+    if (flag === 1) return true
 }
 
 function expandShown(board, elCell,
@@ -131,11 +183,40 @@ function expandShown(board, elCell,
 }
 
 function setRandMinesPoss(board, num) {
-    for (var k = 0; k < num; k++) {
+    var minesPoss = []
+    var minesNum = 0
+    while (minesNum < num) {
         var pos = { i: getRandomIntInclusive(0, ROWS - 1), j: getRandomIntInclusive(0, COLS - 1) }
         if (board[pos.i][pos.j].isMine === false) {
             // console.log('mine pos:', pos);
             board[pos.i][pos.j].isMine = true;
+            minesPoss.push(pos)
+            minesNum++
         }
     }
+    return minesPoss
+}
+
+function onSetLevel(elLevel) {
+    const gameLevel = elLevel.classList[0]
+    switch (gameLevel) {
+        case 'Beginner':
+            ROWS = 4
+            COLS = 4
+            NUM_OF_MINES = 2
+            break;
+        case 'Medium':
+            ROWS = 8
+            COLS = 8
+            NUM_OF_MINES = 14
+            break;
+        case 'Expert':
+            ROWS = 12
+            COLS = 12
+            NUM_OF_MINES = 32
+            break;
+        default:
+            break;
+    }
+    onInitGame()
 }
